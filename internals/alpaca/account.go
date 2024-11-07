226a9c56c0f3c1fd821004d1b0cb326fff9ea71d
José Matías Ortúzar <jortuzar@aligare.cl>
2024-11-07 17:09:21 -0300

    feat: wip: get equity

--- internals/alpaca/account.go
+++ internals/alpaca/account.go
@@ -9,17 +9,21 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-func account() *alpaca.Account {
+// alpacaAccount returns the account details from Alpaca.
+//
+// If there is an error getting the account, the application will exit.
+func alpacaAccount() *alpaca.Account {
 	acc, err := context.GetAlpacaClient().GetAccount()
 
 	if err != nil {
-		fmt.Println("Error getting account. App will exit.", err)
+		fmt.Println("Error getting account. App will exit.")
 		os.Exit(-1)
 	}
 
 	return acc
 }
 
-func AccountEquity() decimal.Decimal {
-	return account().Equity
+// AlpacaAccountEquity returns the account's equity as a decimal.Decimal.
+func AlpacaAccountEquity() decimal.Decimal {
+	return alpacaAccount().Equity
 }
